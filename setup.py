98d1e0ba9d3fabb9f23b58a49cf39eb8f6b026fe
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2017-04-15 21:15:43 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py-bcrypt',
+    version='1.0',
+    description='A native python bcrypt',
+    url='https://github.com/fulfilio/py-bcrypt',
+    py_modules=['bcrypt', 'blowfish'],
+)
